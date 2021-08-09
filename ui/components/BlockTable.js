ea52636c47b45516c0bc4f408c7b9ac52380d219
sigoden <sigoden@gmail.com>
2021-08-09 10:55:36 +0800

    split blocks table

---
+++ ui/components/BlockTable.js
@@ -0,0 +1,54 @@
+import { Table } from "antd";
+import Link from "next/link";
+import { ecllipseHash } from "../lib/utils";
+import TimeAgo from "../components/TimeAgo";
+import FinalizedStatus from "../components/FinializeStatus";
+import styles from "./BlockTable.module.css";
+
+const columns = [
+  {
+    title: "Block",
+    dataIndex: "blockNum",
+    render: blockNum => <Link href={`/blocks/${blockNum}`}><a>{blockNum}</a></Link>
+  },
+  {
+    title: "Status",
+    dataIndex: "finalized",
+    render: finalized => <FinalizedStatus finalized={finalized} />,
+  },
+  {
+    title: "Time",
+    dataIndex: "blockAt",
+    render: blockAt => <TimeAgo time={blockAt} />
+  },
+  {
+    title: "Extrinsics",
+    dataIndex: "extrinsicsCount",
+    render: (extrinsicsCount, record) => extrinsicsCount > 0 ? <Link href={`/blocks/${record.blockNum}?tab=extrinsics`}><a>{extrinsicsCount}</a></Link> : extrinsicsCount,
+  },
+  {
+    title: "Events",
+    dataIndex: "eventsCount",
+    render: (eventsCount, record) => eventsCount > 0 ? <Link href={`/blocks/${record.blockNum}?tab=events`}><a>{eventsCount}</a></Link> : eventsCount,
+  },
+  {
+    title: "Validator",
+    dataIndex: "validator",
+    render: hash => <Link href={`/accounts/${hash}`}><a>{ecllipseHash(hash)}</a></Link>,
+  },
+  {
+    title: "Block hash",
+    dataIndex: "blockHash",
+    render: (hash, record) => <Link href={`/blocks/${record.blockNum}`}><a>{ecllipseHash(hash)}</a></Link>,
+  }
+]
+
+
+export default function BlockTable(props) {
+  return (
+    <Table 
+      columns={columns}
+      rowKey="blockNum"
+      {...props} />
+  );
+}
--- ui/pages/blocks.js
+++ ui/pages/blocks.js
@@ -1,53 +1,11 @@
-import { Table } from "antd";
-import Link from "next/link";
 import { useRequest } from "@umijs/hooks";
-import { ecllipseHash } from "../lib/utils";
-import TimeAgo from "../components/TimeAgo";
+import BlockTable from "../components/BlockTable";
 import MainLayout from "../components/MainLayout";
-import FinalizedStatus from "../components/FinializeStatus";
 
 async function listExtrinsics(paginatedParams) {
   return fetch(`/api/blocks?current=${paginatedParams.current}&pageSize=${paginatedParams.pageSize}`)
     .then((res) => res.json())
 }
-
-const columns = [
-  {
-    title: "Block",
-    dataIndex: "blockNum",
-    render: blockNum => <Link href={`/blocks/${blockNum}`}><a>{blockNum}</a></Link>
-  },
-  {
-    title: "Status",
-    dataIndex: "finalized",
-    render: finalized => <FinalizedStatus finalized={finalized} />,
-  },
-  {
-    title: "Time",
-    dataIndex: "blockAt",
-    render: blockAt => <TimeAgo time={blockAt} />
-  },
-  {
-    title: "Extrinsics",
-    dataIndex: "extrinsicsCount",
-    render: (extrinsicsCount, record) => extrinsicsCount > 0 ? <Link href={`/blocks/${record.blockNum}?tab=extrinsics`}><a>{extrinsicsCount}</a></Link> : extrinsicsCount,
-  },
-  {
-    title: "Events",
-    dataIndex: "eventsCount",
-    render: (eventsCount, record) => eventsCount > 0 ? <Link href={`/blocks/${record.blockNum}?tab=events`}><a>{eventsCount}</a></Link> : eventsCount,
-  },
-  {
-    title: "Validator",
-    dataIndex: "validator",
-    render: hash => <Link href={`/accounts/${hash}`}><a>{ecllipseHash(hash)}</a></Link>,
-  },
-  {
-    title: "Block hash",
-    dataIndex: "blockHash",
-    render: (hash, record) => <Link href={`/blocks/${record.blockNum}`}><a>{ecllipseHash(hash)}</a></Link>,
-  }
-]
 
 export default function BlocksPage() {
   const { tableProps } = useRequest(params => listExtrinsics(params), {
@@ -55,8 +13,8 @@ export default function BlocksPage() {
     defaultPageSize: 20,
   });
   return (
-    <Table columns={columns} rowKey="blockNum" {...tableProps} />
-  );
+    <BlockTable {...tableProps} />
+  )
 }
 
 BlocksPage.getLayout = (page) => (
